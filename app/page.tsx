1c1bc2397efeae46166fa9bc9f6d607821d4fef0
Falasefemi2 <femifalase228@gmail.com>
2024-07-17 22:06:34 +0100

    feat: Added component for date
    Message User can insert date range for campaign

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,15 @@
-import Link from "next/link"
-import {
-  Bell,
-  CircleUser,
-  LineChart,
-  Menu,
-  Package,
-  Package2,
-  Search,
-  ShoppingCart,
-  Users,
-} from "lucide-react"
 
-import { Badge } from "@/components/ui/badge"
+import { DateOverview } from "@/components/DateOverview";
 import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Input } from "@/components/ui/input"
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
-import Sidebar from "@/components/Sidebar"
-import MobileNav from "@/components/MobileNav"
-import Navbar from "@/components/Navbar"
+
 
 export default function Home() {
   return (
 
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-      <div className="flex items-center">
-        <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-lg font-semibold md:text-2xl">Overview</h1>
+        <DateOverview />
       </div>
       <div
         className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1"
